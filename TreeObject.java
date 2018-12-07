bf3863b10dead265cf8ff2cb825692cb8025e569
D0P3ST <43076785+D0P3ST@users.noreply.github.com>
2018-12-06 17:20:23 -0700

    Update TreeObject.java

--- TreeObject.java
+++ TreeObject.java
@@ -46,7 +46,76 @@ public class TreeObject<T> implements Comparable<TreeObject>
     }
 
     public String toString()
+    {/**
+ * The object class used by BTree
+ *
+ * 
+ */
+public class TreeObject<T> implements Comparable<TreeObject>
+{
+    private int frequency;
+    private long data;
+
+    /**
+     * @param data
+     * @param frequency
+     */
+    public TreeObject(long data, int frequency)
     {
+        this.data = data;
+        this.frequency = frequency;
+    }
+
+    /**
+     * @param data
+     */
+    public TreeObject(long data)
+    {
+        this.data = data;
+        this.frequency = 1;
+    }
+
+    /**
+     * updates frequency
+     */
+    public void increaseFrequency()
+    {
+        this.frequency++;
+    }
+
+    /**
+     * @return frequency
+     */
+    public int getFrequency()
+    {
+        return frequency;
+    }
+
+    /**
+     * @return data
+     */
+    public long getData()
+    {
+        return data;
+    }
+
+    public int compareTo(TreeObject that)
+    {
+        if (this.data < that.data)
+            return -1;
+        else if (this.data > that.data)
+            return 1;
+        else
+            return 0;
+    }
+
+    public String toString()
+    {
+        String s = new String();
+        s += "Key: " + data + " Frequency: " + frequency;
+        return s;
+    }
+}
         String s = new String();
         s += "Key: " + data + " Frequency: " + frequency;
         return s;
