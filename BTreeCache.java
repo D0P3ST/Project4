f25daf42fa189bfdd29e895d6a38e3990ed13acb
D0P3ST <43076785+D0P3ST@users.noreply.github.com>
2018-12-08 16:11:05 -0700

    Update BTreeCache.java

--- BTreeCache.java
+++ BTreeCache.java
@@ -8,54 +8,6 @@ import java.util.LinkedList;
  */
 public class BTreeCache implements Iterable<BTreeNode>
 {
-	 private LinkedList<BTreeNode> cacheList;
-	private int numHits, numMisses;
-    private final int MAX_CACHE_SIZE;
-   	private int size;
-   
-
-    public BTreeCache(int MAX_SIZE)
-    {
-        this.size=0;
-    	this.MAX_CACHE_SIZE = MAX_SIZE;
-        cacheList = new LinkedList<BTreeNode>();
-    }
-
-   
-
-    public void clearCache()
-    {
-        cacheList.clear();
-    }
-
-   
-    public BTreeNode readNode(int nodeOffset)
-    {
-        for (BTreeNode n : cacheList)
-        {
-            if (n.getOffset() == nodeOffset)
-            {
-                cacheList.remove(n); // the remove method of linked list
-                cacheList.addFirst(n); // the add method of linked list
-                increaseNumHits();
-                return n;
-            }
-        }
-        increaseNumMisses();
-        return null;
-    }
-
-    public BTreeNode add(BTreeNode nodeToAdd, int offset)
-    {import java.util.Iterator;
-import java.util.LinkedList;
-
-/**
- * The cache class is implemented for the sake of the Cache
- *
- * 
- */
-public class BTreeCache implements Iterable<BTreeNode>
-{
 	private LinkedList<BTreeNode> cacheList;
 	private int numHits, numMisses;
     private final int MAX_CACHE_SIZE;
