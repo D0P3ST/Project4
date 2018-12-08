107a11c2c7902c64f076ad7c30e2bdf2ffe06f64
seanycash10 <43229039+seanycash10@users.noreply.github.com>
2018-12-08 16:02:09 -0700

    Update TreeObject.java
    fixed treeobject

--- TreeObject.java
+++ TreeObject.java
@@ -8,54 +8,6 @@ public class TreeObject<T> implements Comparable<TreeObject>
     private int frequency;
     private long data;
 
-    public TreeObject(long data, int frequency)
-    {
-        this.data = data;
-        this.frequency = frequency;
-    }
-
-    public TreeObject(long data)
-    {
-        this.data = data;
-        this.frequency = 1;
-    }
-
-    public void increaseFrequency()
-    {
-        this.frequency++;
-    }
-
-    public int getFrequency()
-    {
-        return frequency;
-    }
-
-    public long getData()
-    {
-        return data;
-    }
-
-    public int compareTo(TreeObject that)
-    {
-        if (this.data < that.data)
-            return -1;
-        else if (this.data > that.data)
-            return 1;
-        else
-            return 0;
-    }
-
-    public String toString()
-    {/**
- * The object class used by BTree
- *
- * 
- */
-public class TreeObject<T> implements Comparable<TreeObject>
-{
-    private int frequency;
-    private long data;
-
     /**
      * @param data
      * @param frequency
@@ -115,9 +67,5 @@ public class TreeObject<T> implements Comparable<TreeObject>
         s += "Key: " + data + " Frequency: " + frequency;
         return s;
     }
-}
-        String s = new String();
-        s += "Key: " + data + " Frequency: " + frequency;
-        return s;
-    }
+   
 }
